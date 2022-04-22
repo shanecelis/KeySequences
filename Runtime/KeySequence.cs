42d301d22d71908ada597727102c5c455982e363
Shane Celis <shanecelis@thinboi.hsd1.vt.comcast.net>
2022-04-22 06:18:07 -0400

    feature: Add KeySequence class and Trie dll.

---
+++ Runtime/KeySequence.cs
@@ -0,0 +1,69 @@
+using System;
+using System.Text;
+using rm.Trie;
+
+namespace SeawispHunter.KeySequences {
+/* KeySequences
+
+ This class was made to interoperate with Unity's InputSystem
+ */
+public class KeySequence {
+  TrieMap<Action<string>> trie = new TrieMap<Action<string>>();
+  private StringBuilder keyAccum = new StringBuilder();
+  public Action<string> defaultAction = null;
+  public Action<string> overrideAction = null;
+  public bool enabled { get; private set; } = false;
+
+  public void Enable() => enabled = true;
+  public void Disable() => enabled = false;
+
+  public string accumulated => keyAccum.ToString();
+
+  public Action<string> this[string key] {
+    get => trie.ValueBy(key);
+    set => trie.Add(key, value);
+  }
+
+  public void Add(string key, Action<string> action = null) {
+    trie.Add(key, action);
+  }
+
+  public bool HasKey(string key) => trie.HasKey(key);
+
+  public bool HasKeyPrefix(string key) => trie.HasKeyPrefix(key);
+
+  public bool TryGetValue(string key, out Action<string> action, out bool hasPrefix) {
+    hasPrefix = trie.HasKeyPrefix(key);
+    action = trie.ValueBy(key);
+    return trie.HasKey(key);
+  }
+
+  public void OnTextInput(char c) {
+    if (! enabled)
+      return;
+    keyAccum.Append(c);
+    var key = keyAccum.ToString();
+    if (TryGetValue(key, out var action, out bool hasPrefix)) {
+      // We have a key.
+      if (overrideAction != null)
+        overrideAction(key);
+      else if (action != null)
+        action(key);
+      else if (defaultAction != null)
+        defaultAction(key);
+    } else if (! hasPrefix) {
+      // No key and no prefix. We could be starting a new input.
+      if (keyAccum.Length > 1) {
+        // Re-evaluate with a clean slate.
+        keyAccum.Clear();
+        // Recurse. Will only ever be one call deep.
+        OnTextInput(c);
+        return;
+      }
+    }
+    if (! hasPrefix)
+      keyAccum.Clear();
+  }
+}
+}
+
