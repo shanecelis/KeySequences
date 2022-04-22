54b412c0f3ff86da82a209420098c208f2aac652
Shane Celis <shanecelis@thinboi.hsd1.vt.comcast.net>
2022-04-22 06:29:04 -0400

    feature: Add Demo Key Sequence sample.

---
+++ Samples~/Demo Key Sequence/KeySequenceExample.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using SeawispHunter.KeySequences;
+using UnityEngine.InputSystem;
+
+public class KeySequenceExample : MonoBehaviour {
+    KeySequence ks = new KeySequence();
+    public string[] keys;
+    Label label;
+    // Start is called before the first frame update
+    void Start() {
+        var root = GetComponent<UIDocument>().rootVisualElement;
+        label = root.Q<Label>();
+        foreach (var key in keys)
+            ks.Add(key);
+        Keyboard.current.onTextInput += OnTextInput;
+        ks.defaultAction += key => Debug.Log("action " + key);
+        ks.Enable();
+    }
+
+    void OnTextInput(char c) {
+        ks.OnTextInput(c);
+        label.text = "here " + ks.accumulated;
+    }
+
+}
