e22f813adeb26eb114279e9f7c88f3d21a2a3262
Shane Celis <shane.celis@gmail.com>
2022-04-28 05:02:12 -0400

    feature: Add sample using UIToolkit.

---
+++ Samples~/Demo UIToolkit/KeySequenceExample.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+using SeawispHunter.KeySequences;
+using UnityEngine.InputSystem;
+using System.ComponentModel;
+
+public class KeySequenceExample : MonoBehaviour {
+  public KeySequencerMapInt keySequencer;
+  Label label;
+
+  void OnEnable() {
+    keySequencer.Enable();
+    Keyboard.current.onTextInput += keySequencer.Input;
+  }
+
+  void OnDisable() {
+    keySequencer.Disable();
+    Keyboard.current.onTextInput -= keySequencer.Input;
+  }
+
+  void Start() {
+#if ! ENABLE_INPUT_SYSTEM
+    Debug.LogError("New input system required.");
+#else
+    Debug.Log("Using new input system.");
+    var root = GetComponent<UIDocument>().rootVisualElement;
+    label = root.Q<Label>();
+    keySequencer.accept += (_key, value) => {
+      Debug.Log($"action {_key} value {value}");
+    };
+    keySequencer.propertyChanged += OnPropertyChange;
+#endif
+  }
+
+  void OnPropertyChange(object sender, PropertyChangedEventArgs args) {
+    label.text = "here " + keySequencer.accumulated;
+  }
+
+}
