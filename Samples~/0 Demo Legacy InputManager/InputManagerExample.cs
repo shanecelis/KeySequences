a2de046ad94b02616422c2c17db7fee4eccdeae1
Shane Celis <shane.celis@gmail.com>
2022-05-01 01:44:33 -0400

    refactor: Normalize legacy 0 demo to 1 demo.

--- Samples~/0 Demo Legacy InputManager/InputManagerExample.cs
+++ Samples~/0 Demo Legacy InputManager/InputManagerExample.cs
@@ -7,14 +7,18 @@ using System.ComponentModel;
 
 public class InputManagerExample : MonoBehaviour {
   public KeySequencer keySequences;
+  [Header("Turn off logging (causes allocations)")]
+  public bool enableLogging = true;
   public Text label;
 
   void OnEnable() {
     keySequences.Enable();
+    keySequences.propertyChanged += OnPropertyChange;
   }
 
   void OnDisable() {
     keySequences.Disable();
+    keySequences.propertyChanged -= OnPropertyChange;
   }
 
   void Start() {
@@ -24,23 +28,38 @@ public class InputManagerExample : MonoBehaviour {
     Debug.Log("Using old input system.");
     Debug.Log($"There are {keySequences.Count} key sequences.");
     keySequences.accept += _key => {
-      var msg = "ACCEPT " + _key;
+      var msg = $"ACCEPT {_key}";
       Debug.Log(msg);
       label.text = msg;
     };
-    keySequences.propertyChanged += OnPropertyChange;
 #endif
   }
 
-  /** We have to do this 60 times a second to make sure we don't miss a key.
-      Boo. */
+  /** You can use keySequences with the legacy InputManager. However, there are
+      a few caveats.
+
+      You have to poll 60 times a second to make sure we don't miss a key. Boo.
+      Another disadvantage of this method is that calling `Input.inputString`
+      causes Unity to allocate a string. So there are three strikes against
+      this:
+
+      - It allocates unavoidably for every keypress that contains key presses.
+      - It runs every frame.
+      - It's a pull rather than a push.
+
+      Each of these are mitigated by the new InputSystem shown in demo 1. But the code
+      works and it probably won't be an issue if you're already deeply invested
+      in the legacy InputManager.
+  */
   void Update() {
     foreach (var c in Input.inputString) {
       keySequences.Input(c);
-      if (! char.IsControl(c))
-        Debug.Log($"Input char '{c}' int {(int) c}");
-      else
-        Debug.Log($"Input char non-printable int {(int) c}");
+      if (enableLogging) {
+        if (! char.IsControl(c))
+          Debug.Log($"Input char '{c}' int {(int) c}");
+        else
+          Debug.Log($"Input char non-printable int {(int) c}");
+      }
     }
   }
 
